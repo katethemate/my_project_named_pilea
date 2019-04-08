e28cb2648d47691554f55ef8ad6bb5ef1dd1b842
kate <katja.roth@code.berlin>
2019-04-08 17:44:21 +0200

    port2

--- app/__init__.py
+++ app/__init__.py
@@ -34,6 +34,7 @@ def create_app(test_config=None):
         return render_template('first_scenario.html')
 
     return app
+create_app()
 
 if __name__ == '__main__':
       app.run(host='0.0.0.0', port=80)
