27a5990aa101bf221c17ad12eab9f4c0aa38e3e5
kate <katja.roth@code.berlin>
2019-04-18 14:24:46 +0200

    trying smoothscroll

--- app/static/home.js
+++ app/static/home.js
@@ -1,7 +1,40 @@
-
 document.addEventListener("DOMContentLoaded", function() {
 
-    function change() {
-      var image= document.getElementById('')
+    window.onscroll = function() {
+    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+        document.getElementById("logo").style.width = "10%";
+    } else {
+        document.getElementById("logo").style.width = "30%";
+    }
+    }
+
+
+    function scroll() {
+        window.scrollTo(500, 0);
+    }
+        
+
+
+    window.smoothScroll = function(target) {
+        var scrollContainer = target;
+        do { //find scroll container
+            scrollContainer = scrollContainer.parentNode;
+            if (!scrollContainer) return;
+            scrollContainer.scrollTop += 1;
+        } while (scrollContainer.scrollTop == 0);
+
+        var targetY = 0;
+        do { //find the top of target relatively to the container
+            if (target == scrollContainer) break;
+            targetY += target.offsetTop;
+        } while (target = target.offsetParent);
+
+        scroll = function(c, a, b, i) {
+            i++; if (i > 30) return;
+            c.scrollTop = a + (b - a) / 30 * i;
+            setTimeout(function(){ scroll(c, a, b, i); }, 20);
+        }
+        // start scrolling
+        scroll(scrollContainer, scrollContainer.scrollTop, targetY, 0);
     }
 });
