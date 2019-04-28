c17ad8bf0bbaadd931a087b48289fc23d8522fc6
kate <katja.roth@code.berlin>
2019-04-28 18:38:42 +0200

    logo scaling proportional with scrolling, and navbar works kind of

--- app/static/home.js
+++ app/static/home.js
@@ -1,40 +1,87 @@
+
 document.addEventListener("DOMContentLoaded", function() {
+  var height = window.innerHeight;
+ 
 
-    window.onscroll = function() {
-    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
-        document.getElementById("logo").style.width = "10%";
-    } else {
-        document.getElementById("logo").style.width = "30%";
+  window.addEventListener("scroll", function(){
+    var x;
+    var difference;
+    var topCoordinate = document.getElementById("home").getBoundingClientRect().top * (-1);
+    
+    difference = height - topCoordinate;
+    x = 20/difference;
+
+    width = 10 + x - topCoordinate;
+
+    width += "%";
+    
+    //console.log(width)
+    if (document.body.scrollTop < document.getElementById("home").getBoundingClientRect().height || document.documentElement.scrollTop < document.getElementById("home").getBoundingClientRect().height){
+      console.log("works");
+      document.getElementById("logo").style.width = width;  
     }
+  });
+
+  document.getElementById("logo").addEventListener("click", function(){
+    if(document.getElementById("mySidenav").style.width == "0px"){
+      document.getElementById("mySidenav").style.width = "30%";
     }
-
-
-    function scroll() {
-        window.scrollTo(500, 0);
+    else{
+      document.getElementById("mySidenav").style.width = "0px";
     }
-        
+  });
 
-
-    window.smoothScroll = function(target) {
-        var scrollContainer = target;
-        do { //find scroll container
-            scrollContainer = scrollContainer.parentNode;
-            if (!scrollContainer) return;
-            scrollContainer.scrollTop += 1;
-        } while (scrollContainer.scrollTop == 0);
-
-        var targetY = 0;
-        do { //find the top of target relatively to the container
-            if (target == scrollContainer) break;
-            targetY += target.offsetTop;
-        } while (target = target.offsetParent);
-
-        scroll = function(c, a, b, i) {
-            i++; if (i > 30) return;
-            c.scrollTop = a + (b - a) / 30 * i;
-            setTimeout(function(){ scroll(c, a, b, i); }, 20);
+  var animateLogo = function() {
+      var logo;
+      var windowHeight;
+      function init() {
+        logo = document.querySelector('#logo');
+        windowHeight = window.innerHeight;
+        addEventHandlers();
+        checkPosition();
+      }
+      function addEventHandlers() {
+        window.addEventListener('scroll', checkPosition);
+        window.addEventListener('resize', init);
+      }
+      function checkPosition() {
+          var positionFromTop = logo.getBoundingClientRect().top;
+          if (positionFromTop - windowHeight <= 0) {
+            logo.className = logo.className.replace(
+              'hidden',
+              'zoomOut'
+            );
+          }
         }
-        // start scrolling
-        scroll(scrollContainer, scrollContainer.scrollTop, targetY, 0);
+      
+      return {
+        init: init
+      }
     }
+    
+  animateLogo().init();
+
+  
+  
+  var arrows = document.getElementsByClassName('down');
+  
+  for (var i = 0; i < arrows.length; i++) {
+    arrows[i].addEventListener('click', function(){
+      window.scrollBy(0, height);
+    });
+  }
+
+  function loadDoc() {
+      var xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+          document.getElementById("doodle1").innerHTML = this.responseText;
+      
+        }
+      };
+      xhttp.open("GET", "", true);
+      xhttp.send();
+  }
+
+
 });
